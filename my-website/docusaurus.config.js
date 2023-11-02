1995a299b15bb340253f5f92c504ab7e44d69f0c
kliganN <sikpro1337@yandex.ru>
2023-11-02 12:30:09 +0300

    added

--- my-website/docusaurus.config.js
+++ my-website/docusaurus.config.js
@@ -127,7 +127,7 @@ const config = {
             ],
           },
         ],
-        copyright: `Copyright © ${new Date().getFullYear()} My Project, Inc. Built with Docusaurus.`,
+        copyright: `Copyright © ${new Date().getFullYear()} Community docs for people. www.subnetwork.ru`,
       },
       prism: {
         theme: lightCodeTheme,
